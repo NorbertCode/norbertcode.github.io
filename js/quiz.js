72e9ce15d530d71891c8ffa880d3bc6e8e0f1819
NorbertCode <54719382+NorbertCode@users.noreply.github.com>
2023-03-17 17:42:58 +0100

    Special characters don't matter now

--- js/quiz.js
+++ js/quiz.js
@@ -116,15 +116,26 @@ function checkAnswer()
 {
 	let userInput = document.getElementById("input").value;
 	let feedback = document.getElementById("feedback");
-	
-	if (userInput.toLowerCase() == correctAnswer.toLowerCase())
+
+	// inputChars are converted to outputChars so special characters don't matter
+	const inputChars = [',', '.', "?", 'ą', 'ć', 'ę', 'ł', 'ń', 'ó', 'ś', 'ź', 'ż', 'ä', 'ö', 'ü', 'ß'];
+	const outputChars = ['', '', "", 'a', 'c', 'e', 'l', 'n', 'o', 's', 'z', 'z', 'a', 'o', 'u', 'ss'];
+
+	let processedCorrectAnswer = correctAnswer; // Temporary variable used as a clone of correctAnswer, so correctAnswer still has special characters
+	for (let i = 0; i < inputChars.length; i++)
+	{
+		userInput = replaceAll(userInput, inputChars[i], outputChars[i]);
+		processedCorrectAnswer = replaceAll(processedCorrectAnswer, inputChars[i], outputChars[i]);
+	}
+
+	if (userInput.toLowerCase() == processedCorrectAnswer.toLowerCase())
 	{
 		feedback.innerHTML = "Świetnie! Poprawna odpowiedź.";
 		feedback.style.color = "#1fad45";
 	}
 	else
 	{
-		feedback.innerHTML = "Niestety, poprawna odpowiedź to " + correctAnswer;
+		feedback.innerHTML = "Niestety, poprawna odpowiedź to \'" + correctAnswer + "\'";
 		feedback.style.color = "#d60b33";
 	}
 	
@@ -146,3 +157,10 @@ function getRandomInt(max) // Exclusive
 {
 	return Math.floor(Math.random() * max);
 }
+
+// The only way to replace all instances of a certain character in a string is using regex,
+// but for some reason when I put a variable in a regexp it returned nothing, so this is another way to do that
+function replaceAll(string, search, replacement) 
+{
+    return string.split(search).join(replacement);
+};
